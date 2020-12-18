8cf6d6ce2361850695ebc23a0664ad223b34aeae
liquid-finance <liquid.finance.dev@gmail.com>
2020-12-18 09:33:51 -0800

    Hardcode timer

--- src/routes/Event/components/EventHeader.js
+++ src/routes/Event/components/EventHeader.js
@@ -8,18 +8,6 @@ import { EventTitle } from './EventTitle'
 export const EventHeader = ({ stats }) => {
   const { activate, active, claim } = useLGE()
 
-  const getTime = () => {
-    
-    if (stats && stats.length) {
-      console.log(new Date(stats[7] * 1000).toLocaleTimeString())
-      const date = new Date(stats[7] * 1000)
-      console.log(date.toISOString())
-      return date.toISOString()
-    } else {
-      return '0'
-    }
-  }
-
   const onActivate = async () => {
     const txHash = await activate()
 
@@ -32,44 +20,34 @@ export const EventHeader = ({ stats }) => {
   return (
     <Grid container direction="column" justify="center" align="center">
       <Grid item>
-        {stats && stats.length ? (
+        {stats && stats.length && (stats[7] * 1000 < Date.now()) ? (
           <>
-            {(stats[7] * 1000 < Date.now()) ? (
+            {active ? (
+              <Button
+                color="secondary"
+                variant="contained"
+                onClick={onActivate}
+              >
+                ACTIVATE LGE
+              </Button>
+            ) : (
               <>
-                {active ? (
+                {new BigNumber(stats[8]).gt(0) && (
                   <Button
                     color="secondary"
                     variant="contained"
-                    onClick={onActivate}
+                    onClick={onClaim}
                   >
-                    ACTIVATE LGE
+                    CLAIM
                   </Button>
-                ) : (
-                  <>
-                    {new BigNumber(stats[8]).gt(0) && (
-                      <Button
-                        color="secondary"
-                        variant="contained"
-                        onClick={onClaim}
-                      >
-                        CLAIM
-                      </Button>
-                    )}
-                  </>
                 )}
-              </>
-            ) : (
-              <>
-              {/* {stats[7] * 1000}
-              {Date.now()} */}
-              <CountdownClock endDate={getTime()} />
               </>
             )}
           </>
-        ) : (
-          <CountdownClock endDate={'0'} />
-        )}
-      </Grid>
+          ) : (
+            <CountdownClock endDate={new Date(1608400682000).toISOString()} />
+          )}
+        </Grid>
       <EventTitle title="Total Contributed" data={stats && stats.length ? stats[6] : '0'} />
     </Grid>
   )
--- src/routes/Event/components/EventTitle.js
+++ src/routes/Event/components/EventTitle.js
@@ -1,10 +1,32 @@
 import { Typography } from '@material-ui/core'
 import BigNumber from 'bignumber.js'
 import { ColumnView } from 'components/ColumnView'
-import React from 'react'
-import { getDisplayBalanceFixed } from 'utils'
+import React, { useCallback,useEffect } from 'react'
+import { getDisplayBalanceFixed, sleep } from 'utils'
+
+const ENDPOINT = "https://assets.altitude.finance/lgebalance/pwdr_lge_balance.json"
 
 export const EventTitle = ({ title, data }) => {
+
+  const getContribution = useCallback(async () => {
+    const contribution = await fetch(ENDPOINT, { 
+      method: 'GET',
+      mode: 'cors',
+      cache: 'no-cache',
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*"
+      }
+    }).then(r => r.json()).then(d => d)
+    const json = JSON.stringify(contribution)
+    console.log(json)
+  }, [])
+
+  useEffect(() => {
+    getContribution()
+    sleep(1000)
+  }, [])
+
   return (
     <ColumnView mb={2}>
       <Typography variant="h2" align="center">
